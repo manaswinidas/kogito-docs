faabf31847231beb6145a097416d057c283f3318
Manaswini <dasmanaswini10@gmail.com>
2020-05-08 00:56:36 +0530

    fix sed command to replace branch

--- script.sh
+++ script.sh
@@ -4,4 +4,5 @@ read -p "Enter the branch that you want to build:"  branch
 git checkout $branch
 git checkout master -- .travis.yml
 sed -i 's/^#//g' .travis.yml
-sed 's/origin HEAD: [^ ]+ /origin HEAD: "$branch" /' .travis.yml
+# sed 's/origin HEAD: [^ ]+ /origin HEAD: "$branch" /' .travis.yml
+sed 's/origin HEAD: [^ ]\+ /origin HEAD: "$branch" /' .travis.yml
