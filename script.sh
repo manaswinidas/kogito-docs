06ea7ecff711a40db6ea75fa2deb6e728a7a491b
Manaswini <dasmanaswini10@gmail.com>
2020-05-08 00:37:26 +0530

    add script to enable travis build on specific branch

---
+++ script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+read -p "Enter the branch that you want to build:"  branch
+git checkout $branch
+git checkout master -- .travis.yml
+sed -i 's/^#//g' .travis.yml
+sed 's/origin HEAD: [^ ]+ /origin HEAD: $branch /' .travis.yml
